80e59f605a417def96970576cba887e23c33de78
VladKoldov <59209052+VladKoldov@users.noreply.github.com>
2019-12-24 21:21:08 +0200

    Add files via upload
    основное и дополнительное домашнее задание №1

---
+++ Dz1.java
@@ -0,0 +1,9 @@
+public class Dz1 {
+    public static void main(String[] args){
+        System.out.println("* * * * *    * * * *         *           *           *");
+        System.out.println("*            *     *        *  *         *   *    *  *");
+        System.out.println("* *          * * * *       *    *        *      *    *");
+        System.out.println("*            *            * * * * *      *           *");
+        System.out.println("* * * * *    *           *         *     *           *");
+    }
+}
---
+++ Dz11.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Dz11 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+        System.out.println("" + c + "  " + c + "    " + c +  c + c + c + c + "   " + c + "       " + c + "       " + c + c + c + c + c );
+        System.out.println("" + c + "  " + c + "    " + c + "       " + c + "       " + c + "       " + c + "   "  + c );
+        System.out.println("" + c + c + c + c +"    " + c + c + c + "     " + c + "       " + c + "       " + c + "   " + c );
+        System.out.println("" + c + "  " + c + "    " + c + "       " + c + "       " + c + "       " + c + "   " + c );
+        System.out.println("" + c + "  " + c + "    " + c + c + c + c +c + "   " + c + c + c + c + c + "   " + c + c + c + c + c + "   " + c + c + c + c + c );
+    }
+}
